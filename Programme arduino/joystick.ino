54e121ac820fbd739146e3763b63181604f7702a
David <cartier.dc385@gmail.com>
2021-05-03 09:53:05 +0200

    Joystick program

--- Programme arduino/joystick.ino
+++ Programme arduino/joystick.ino
@@ -3,49 +3,32 @@
 
 
 
-//Constants
-#define NUM_JOY 2
-#define MIN_VAL 0
-#define MAX_VAL 1023
-
-//Parameters
-const int joyPin [2] = {A0, A1};
-const int joyBtn  = 2;
-const int joyOffset  = 0;
-
-//Variables
-int joyVal [NUM_JOY] = {0, 0};
+int dataIn[4] {0,0,0,0};
+int in_byte = 0;
+int array_index;
 
 void setup() {
-  //Init Serial USB
-  Serial.begin(9600);
-  Serial.println(F("Initialize System"));
-  //Init Joystick
-  for (int i = 0; i < NUM_JOY; i++) pinMode(joyPin[i], INPUT);
+  Serial.begin (9600);
 }
 
 void loop() {
-  readJoystick();
-  delay(500);
-}
-
-void readJoystick( ) { /* function readJoystick */
-  ////Test routine for Joystick
-  for (int i = 0; i < NUM_JOY; i++) {
-    joyVal[i] = analogRead(joyPin[i]);
-    Serial.print(F("joy")); Serial.print(i); Serial.print(F(" : ")); Serial.println(joyVal[i]);
+  if (Serial.available() > 0) {
+    in_byte ==Serial.read();
+    if (in_byte == (255)) {
+      array_index = 0;
+    }
+    dataIn[array_index] = in_byte;
+    array_index = array_index +1;
   }
-  for (int i = 0; i < NUM_JOY; i++) {
-    Serial.print(F("Conv")), Serial.print(i); Serial.print(F(" : ")); Serial.println(joyRawToPhys(joyVal[i]));
-  }
-  if (!digitalRead(joyBtn)) {
-    Serial.println(F("Joy Button pressed"));
-  }
-}
 
-int joyRawToPhys(int raw) { /* function joyRawToPhys */
-  ////Joystick conversion rule
-  float phys = map(raw, MIN_VAL, MAX_VAL, -100 + joyOffset, 100 + joyOffset) - joyOffset; //
+  Serial.print (dataIn[0]);
+  Serial.print(", button:");
+  Serial.print(dataIn[1]);
+  Serial.print(", x:");
+  Serial.print(dataIn[2]);
+  Serial.print(", y");
+  Serial.print(dataIn[3]);
+  Serial.print(", ");
+  Serial.println (in_byte);
 
-  return phys;
 }
